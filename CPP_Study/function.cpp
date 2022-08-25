658d98599a50ae4a4d1c68631292c00501e4740e
ryutyke <play2ski@gmail.com>
2022-08-26 01:58:00 +0900

    learn call stack (호출 스택)

--- CPP_Study/function.cpp
+++ CPP_Study/function.cpp
@@ -12,7 +12,30 @@ stack frame
 
 */
 
+// 함수 선언부
+void PrintHelloWorld();
+void PrintNumber(int num);
+int MultiplyBy2(int a);
+int MultiplyBy(int a, int b);
 
+int main()
+{
+	PrintNumber(2);
+	cout << MultiplyBy2(5) << endl;
+	cout << MultiplyBy(3, 5) << endl;
+
+	return 0;
+}
+
+// 함수 안에 함수, 그 함수 안에 함수, 그 함수 안에 함수
+// 이런식으로 가다보면 나중에 중단점까지 디버깅할 때, 내가 어떤 경로에서 이 함수를 사용하고 있는지 파악하기 힘들다
+// 예를 들어, PlayerMove() 함수를 호출중인데, 자동이동에서 넘어온건지 수동이동에서 넘어온건지 등등
+// 이를 디버깅 시 나오는 호출스택을 통해 파악할 수 있다
+// 호출스택은 반환 주소값들을 보여준다 (이 함수가 끝난 후 돌아가야 하는 주소값)
+// 그 주소값이 어떤 함수에 있는 주소값인지 보면 된다!
+
+
+// 함수 구현부
 void PrintHelloWorld()
 {
 	cout << "Hello World" << endl;
@@ -25,19 +48,12 @@ void PrintNumber(int num)
 
 int MultiplyBy2(int a)
 {
+	a = MultiplyBy(a, 3);
 	return a * 2;
 }
 
-int MultyplyBy(int a, int b)
+int MultiplyBy(int a, int b)
 {
+	PrintNumber(a);
 	return a * b;
-}
-
-int main()
-{
-	PrintNumber(2);
-	cout << MultiplyBy2(5) << endl;
-	cout << MultyplyBy(3, 5) << endl;
-
-	return 0;
 }
